e9605473a38bfcc7d70422e37f5e9dacc951dff1
pillar <pillar.liang@bindo.com>
2018-07-31 17:52:53 +0800

    npmup

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
-const creator = require('../lib');
-const { app, listen } = creator();
-app.use(async ctx => {
-  ctx.body = { name: 'bb', dog: 123 };
+const { creator, thread } = require('../lib');
+
+thread('auto-dev', function() {
+  const { app, listen } = creator();
+  app.use(async ctx => {
+    ctx.body = { name: 'bb', dog: 123 };
+  });
+  listen(4100);
 });
 
-listen(4100);
